6eddd4b421881f422fa9aa6d97105ea9afd8a98a
dtsigelnitskiy <dtsigelnitskiy@wv00505.local>
2019-12-04 20:05:34 +0300

    Fix components

--- src/components/Icons/Close.tsx
+++ src/components/Icons/Close.tsx
@@ -6,10 +6,12 @@ export function Close(props: TProps) {
     const color = props.color ?? '#D8D8D8';
     /* eslint-disable max-len */
     return (
-        <svg width={size} height={size} viewBox="0 0 18 18">
+        <svg width={size}
+            height={size}
+            viewBox="0 0 18 18">
             <g fill="none" fillRule="evenodd">
                 <path fill={color}
-                      d="M16.812 2.058L9.87 9.008l6.943 6.95a.616.616 0 0 1-.869.87L9 9.877l-6.943 6.95a.614.614 0 0 1-.87-.87l6.944-6.95-6.943-6.95a.616.616 0 1 1 .869-.87L9 8.138l6.943-6.95a.614.614 0 1 1 .87.87z"/>
+                    d="M16.812 2.058L9.87 9.008l6.943 6.95a.616.616 0 0 1-.869.87L9 9.877l-6.943 6.95a.614.614 0 0 1-.87-.87l6.944-6.95-6.943-6.95a.616.616 0 1 1 .869-.87L9 8.138l6.943-6.95a.614.614 0 1 1 .87.87z" />
             </g>
         </svg>
     );
