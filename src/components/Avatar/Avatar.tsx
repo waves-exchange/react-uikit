5e346557299c9553519fb7fecd52659ded9c9b3b
dtsigelnitskiy <dtsigelnitskiy@wv00505.local>
2019-12-04 17:08:56 +0300

    Fix Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { config, create } from 'identity-img';
-import { Box } from '../Box/Box';
+import { defaultTheme } from 'themes/default';
+import styled from '@emotion/styled';
+import { always, prop } from 'ramda';
+import { TPartialDeep } from 'interface';
 
 
 config({ rows: 8, cells: 8 });
 
 
-export const Avatar = (props: TProps) => {
-    return (
-        <Box overflow="hidden" borderRadius="circle" height={props.size} width={props.size}>
-            <img src={create(props.address, { size: props.size })}
-                 width={props.size}
-                 height={props.size}/>
-        </Box>
-    );
-};
+function getSize(size: TAvatarSizes, theme?: TPartialDeep<Pick<typeof defaultTheme, 'avatarSizes'>>): number {
+    return prop(size, theme?.avatarSizes ?? Object.create(null)) ?? defaultTheme.avatarSizes[size];
+}
 
+const AvatarFunction = ({ address, size = 'medium', theme, ...props }: TProps) => (
+    <img src={create(address, { size: getSize(size, theme) })}
+         width={getSize(size, theme)}
+         height={getSize(size, theme)}
+         {...props}/>
+);
+
+export const Avatar = styled(AvatarFunction, { shouldForwardProp: always(true) })(
+    {
+        overflow: 'hidden',
+        borderRadius: '100%'
+    }
+);
 
 type TProps = {
-    size: number;
+    size?: TAvatarSizes;
+    theme?: TPartialDeep<Pick<typeof defaultTheme, 'avatarSizes'>>;
     address: string;
 };
 
-Avatar.defaultProps = {
-    size: 30
-};
+type TAvatarSizes = keyof typeof defaultTheme.avatarSizes;
