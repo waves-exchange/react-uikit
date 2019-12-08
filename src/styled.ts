f1986a310c35952e8e377ed595754f7ac83117f7
Viktor Voronin <vvoronin@web3tech.ru>
2019-12-09 01:27:18 +0300

    format rest

--- src/styled.ts
+++ src/styled.ts
@@ -2,5 +2,6 @@ import * as s from '@emotion/styled';
 import { CreateStyled } from '@emotion/styled';
 import { defaultTheme } from './themes/default';
 
-
-export const styled: CreateStyled<typeof defaultTheme> = s.default as unknown as CreateStyled<typeof defaultTheme>;
+export const styled: CreateStyled<typeof defaultTheme> = (s.default as unknown) as CreateStyled<
+    typeof defaultTheme
+>;
