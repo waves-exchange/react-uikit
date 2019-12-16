3c6c31e399a69201294cdb4a374ee5c838c28952
Max Axenov <maksenov@wavesplatform.com>
2019-12-16 15:12:49 +0300

    DEXW-2576: short mode

--- src/components/AddressAvatar/AddressAvatar.stories.tsx
+++ src/components/AddressAvatar/AddressAvatar.stories.tsx
@@ -3,20 +3,27 @@ import { storiesOf } from '@storybook/react';
 import { defaultTheme } from '../../themes/default';
 import { ThemeProvider } from 'emotion-theming';
 import { AddressAvatar } from './AddressAvatar';
+import { Box } from '../..';
 
 const stories = storiesOf('AddressAvatar', module);
 
 stories.add('simple', () => (
     <ThemeProvider theme={defaultTheme}>
-        <AddressAvatar
-            address="3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7"
-            name="Coco Jumbo"
-        />
-        <AddressAvatar
-            isShort={false}
-            hasName={false}
-            address="3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7"
-            name="Coco Jumbo"
-        />
+        <Box backgroundColor="main.$800" maxWidth="360px">
+            <Box padding="8px 16px">
+                <AddressAvatar
+                    address="3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7"
+                    name="Coco Jumbo"
+                />
+            </Box>
+            <Box padding="8px 16px">
+                <AddressAvatar
+                    isShort={true}
+                    hasName={false}
+                    address="3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7"
+                    name="Coco Jumbo"
+                />
+            </Box>
+        </Box>
     </ThemeProvider>
 ));
