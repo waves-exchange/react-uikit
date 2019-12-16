4a05a2a6c44482c257af734523a4a07890d3f51b
okanishcheva <okanishcheva@wavesplatform.com>
2019-12-16 18:41:46 +0300

    save spinner

---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Spinner } from './Spinner';
+import { ThemeProvider } from 'emotion-theming';
+import { defaultTheme, Flex } from '../..';
+
+const stories = storiesOf('Spinner', module);
+
+stories.add('simple', () => {
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Flex
+                backgroundColor={defaultTheme.colors.main.$800}
+                height="100vh"
+                justifyContent="center"
+            >
+                <Spinner />
+            </Flex>
+        </ThemeProvider>
+    );
+});
