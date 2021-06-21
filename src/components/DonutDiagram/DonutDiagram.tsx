cf440962e3f264f9f13ee7257d0bfb371508f49e
okanishcheva <okanishcheva@wavesplatform.com>
2021-06-21 14:03:27 +0300

    DonutDiagram add gaps

--- src/components/DonutDiagram/DonutDiagram.tsx
+++ src/components/DonutDiagram/DonutDiagram.tsx
@@ -8,6 +8,11 @@ interface IDiagramData {
     color: string;
 }
 
+interface IGaps {
+    color: string;
+    value?: number;
+}
+
 interface IProps {
     data: Array<IDiagramData>;
     baseColor: string;
@@ -15,6 +20,7 @@ interface IProps {
     size: number;
     animationTime?: number;
     easing?: string;
+    gaps?: IGaps;
 }
 
 const defaultProps = {
@@ -33,6 +39,8 @@ const Svg = styled(Box as BoxAsElement<'svg', SVGBoxProps>)({
     },
 });
 
+const DEFAULT_GAP = 0.01;
+
 export const DonutDiagram: React.FC<BoxProps & IProps> = ({
     data,
     baseColor,
@@ -40,19 +48,51 @@ export const DonutDiagram: React.FC<BoxProps & IProps> = ({
     size,
     animationTime,
     easing,
+    gaps,
 }) => {
     const border: number =
         borderProp === 'none' ? size / 2 : (borderProp as number);
     const circumference = Math.PI * (size - border);
+    const baseData = gaps
+        ? [
+              {
+                  value: circumference,
+                  color: gaps.color,
+                  id: 'gap-base',
+              },
+              {
+                  value: circumference * (1 - (gaps?.value || DEFAULT_GAP)),
+                  color: baseColor,
+                  id: 'base',
+              },
+          ]
+        : [{ value: circumference, color: baseColor, id: 'base' }];
     const sectorsData = data
         .slice()
         .sort(({ value: valueA }, { value: valueB }) => valueA - valueB)
         .reduce((acc, { value: amount, color, id }) => {
-            const sum =
-                amount * circumference + (acc[acc.length - 1]?.value || 0);
+            if (!gaps) {
+                const sum =
+                    amount * circumference + (acc[acc.length - 1]?.value || 0);
 
-            return [...acc, { value: sum, color, id }];
+                return [...acc, { value: sum, color, id }];
+            } else {
+                const { color: gapColor, value = DEFAULT_GAP } = gaps;
+                const sum =
+                    (amount - value) * circumference +
+                    (acc[acc.length - 1]?.value || 0);
+
+                const gap = value * circumference + sum;
+
+                return [
+                    ...acc,
+                    { value: sum, color, id },
+                    { value: gap, color: gapColor, id: `gap-${id}` },
+                ];
+            }
         }, [] as Array<IDiagramData>);
+
+    const circlesData = [...baseData, ...sectorsData.reverse()];
 
     return (
         <Box
@@ -68,18 +108,7 @@ export const DonutDiagram: React.FC<BoxProps & IProps> = ({
                 size={size}
                 xmlns="http://www.w3.org/2000/svg"
             >
-                <Svg
-                    as="circle"
-                    cx={size / 2}
-                    cy={size / 2}
-                    r={size / 2 - border / 2}
-                    sx={{
-                        stroke: baseColor,
-                        strokeWidth: border,
-                        fill: 'none',
-                    }}
-                />
-                {sectorsData.reverse().map(({ value, color, id }) => (
+                {circlesData.map(({ value, color, id }) => (
                     <Svg
                         key={id}
                         as="circle"
